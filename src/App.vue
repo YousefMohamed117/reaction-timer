c09807a7cec14402dbb7af71f8eda486d92fad7c
YOUSEF MOHAMED <you@example.com>
2024-01-08 11:08:06 +0300

    vue deploy files

---
+++ src/App.vue
@@ -0,0 +1,61 @@
+<template>
+ <h1>Yousef Reaction Timer</h1>
+ <button @click="start" :disabled="isPlaying">Play</button>
+ <Block v-if="isPlaying" :delay="delay" @end="endgame" />
+ <Results v-if="showResults" :score="reaction"/>
+</template>
+
+<script>
+import Block from './components/Block.vue'
+import Results from './components/Results.vue'
+export default {
+  name: 'App',
+  data() {
+    return {
+      isPlaying : false,
+      delay : null,
+      reaction:null,
+      showResults:false,
+    }
+  },
+  components: {Block, Results},
+  methods:{
+    start(){
+      this.isPlaying = true,
+      this.delay = 2000 + Math.random() * 5000
+      this.showResults = false
+    },
+    endgame(reaction){
+      this.reaction = reaction
+      this.isPlaying = false
+      this.showResults = true
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #444;
+  margin-top: 60px;
+}
+button{
+  background: #0faf87;
+  color: white;
+  border: none;
+  padding: 8px 16px;
+  border-radius: 4px;
+  font-size: 16px;
+  letter-spacing: 1px;
+  cursor: pointer;
+  margin: 10px;
+}
+button:disabled{
+  opacity: 0.2;
+  cursor: not-allowed;
+}
+</style>
